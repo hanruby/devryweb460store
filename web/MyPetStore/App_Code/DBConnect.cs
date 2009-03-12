b0e04df8ee0eeb42405afd428e119f8eec40db75
ethantd1 <ethantd1@hotmail.com>
2009-03-12 06:31:16 -0700

    Added DBConnect.cs Added connection string to web.config

---
+++ web/MyPetStore/App_Code/DBConnect.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for DBConnect
+/// </summary>
+public class DBConnect
+{
+    private SqlConnection conn;
+    private SqlCommand comm;
+    private string _sConnectionString;
+
+    
+    #region Constructors
+    public DBConnect()
+    {
+        //Default constructor.  Will need to set _sConnectionString manually in the calling class.
+    }
+
+    public DBConnect(string sConnectionString)
+    {
+        sConnectionString = _sConnectionString;
+        conn = new SqlConnection();
+        comm = new SqlCommand();
+        OpenConnection();
+    }
+    #endregion
+
+    #region Methods
+    public void OpenConnection()
+    {
+        conn.ConnectionString = _sConnectionString;
+        conn.Open();
+        comm.Connection = conn;
+    }
+
+    public void ExecSQL(string sSQL)
+    {
+        comm.CommandText = sSQL;
+        comm.ExecuteNonQuery();
+    }
+
+    public SqlDataReader GetDR(string sSQL)
+    {
+        comm.CommandText = sSQL;
+        return comm.ExecuteReader();       
+    }
+
+    public void Close()
+    {
+        comm.Dispose();
+        conn.Close();
+        conn.Dispose();
+    }
+
+    #endregion
+
+    
+    #region Properties
+    public string ConnectionString
+    {
+        get { return _sConnectionString; }
+        set { _sConnectionString = value; }
+    }
+
+    
+    #endregion
+}
