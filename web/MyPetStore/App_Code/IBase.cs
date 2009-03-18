3ba820b7ae296db06ffb700018b5ec721c8737f5
robrobbins <rob.robb.ns@gmail.com>
2009-03-18 10:02:05 -0700

    created IBase Interface

---
+++ web/MyPetStore/App_Code/IBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for IBase
+/// </summary>
+public interface IBase
+{
+    //possible attributes/properties? still an unknown
+
+    //behaviours
+    IList<T> Get();
+    bool Add();
+    bool Delete();
+    void Clear();
+
+
+}
