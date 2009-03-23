2b05c270cc03275a1de9635acea294873ebefbbf
ethantd1 <ethantd1@hotmail.com>
2009-03-23 05:51:12 -0700

    API for OrderTrackinga and OrderVideos.

---
+++ web/MyPetStore/App_Code/OrderTracking.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+/// <summary>
+/// DBAPI for OrderTracking table.
+/// </summary>
+public partial class OrderTracking : IBase
+{
+#region Attributes
+    private string _TrackingID;
+    private int _OrderID;
+    private string _ItemID;
+    private int _VendorID;
+    private DateTime _ShipDate;
+    private DateTime _EstArrival;
+    private string _TrackingURL;
+#endregion
+
+#region Constructors
+    public OrderTracking()
+    {
+        // Default Constructor
+    }
+    
+    public OrderTracking(string p_TrackingID, int p_OrderID, string p_ItemID, int p_VendorID,
+        DateTime p_ShipDate, DateTime p_EstArrival, string p_TrackingURL)
+    {
+        //DBConnect dbc = new DBConnect(ConfigurationManager.AppSettings["MyPetStoreDB"]);
+        _TrackingID = p_TrackingID;
+        _OrderID = p_OrderID;
+        _ItemID = p_ItemID;
+        _VendorID = p_VendorID;
+        _ShipDate = p_ShipDate;
+        _EstArrival = p_EstArrival;
+        _TrackingURL = p_TrackingURL;
+    }
+
+
+
+#endregion
+
+#region Methods
+    public IList<object> Get()
+    {
+        List<object> ot = new List<object>();
+
+        return ot;
+    }
+    
+    public bool Add()
+    {
+        bool s = false;
+
+        return s;
+    }
+    
+    public bool Delete()
+    {
+        bool s = false;
+
+        return s;
+    }
+    
+    public void Clear()
+    {
+        _TrackingID = null;
+        _OrderID = -1;
+        _ItemID = null;
+        _VendorID = -1;
+        _ShipDate = DateTime.MinValue;
+        _EstArrival = DateTime.MinValue;
+        _TrackingURL = null;
+    }
+#endregion
+
+#region Properties
+    public string TrackingID
+    {
+        get{return _TrackingID;}
+        set{_TrackingID = value;}
+    }
+
+    public int OrderID
+    {
+        get{return _OrderID;}
+        set{_OrderID = value;}
+    }
+
+    public string ItemID
+    {
+        get{return _ItemID;}
+        set{_ItemID = value;}
+    }
+
+    public int VendorID
+    {
+        get{return _VendorID;}
+        set{_VendorID = value;}
+    }
+
+    public DateTime ShipDate
+    {
+        get{return _ShipDate;}
+        set{_ShipDate = value;}
+    }
+
+    public DateTime EstArrival
+    {
+        get{return _EstArrival;}
+        set{_EstArrival = value;}
+    }
+
+    public string TrackingURL
+    {
+        get{return _TrackingID;}
+        set{_TrackingURL = value;}
+    }
+#endregion
+
+}
---
+++ web/MyPetStore/App_Code/OrderVideos.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for OrderVideos
+/// </summary>
+public partial class OrderVideos : IBase
+{
+#region Attributes
+    private int _VideoID;
+    private string _ItemID;
+    private int _VendorID;
+    private string _VideoName;
+    private string _VideoDescription;
+    private string _Link;
+    private string _VideoSource;
+#endregion
+
+#region Constructors
+    public OrderVideos()
+	{
+		// Default Constructor
+	}
+
+    public OrderVideos(int p_VideoID, string p_ItemID, int p_VendorID, string p_VideoName,
+        string p_VideoDescription, string p_Link, string p_VideoSource)
+    {
+        _VideoID = p_VideoID;
+        _ItemID = p_ItemID;
+        _VendorID = p_VendorID;
+        _VideoName = p_VideoName;
+        _VideoDescription = p_VideoDescription;
+        _Link = p_Link;
+        _VideoSource = p_VideoSource;
+    }
+#endregion
+
+#region Methods
+    public IList<object> Get()
+    {
+        List<object> ov = new List<object>();
+
+        return ov;
+    }
+
+    public bool Add()
+    {
+        bool s = false;
+
+        return s;
+    }
+
+    public bool Delete()
+    {
+        bool s = false;
+
+        return s;
+    }
+
+    public void Clear()
+    {
+        _VideoID = -1;
+        _ItemID = null;
+        _VendorID = -1;
+        _VideoName = null;
+        _VideoDescription = null;
+        _Link = null;
+        _VideoSource = null;
+        // List(T).Clear()??
+    }
+#endregion
+
+#region Properties
+    public int VideoID
+    {
+        get { return _VideoID; }
+        set { _VideoID = value; }
+    }
+
+    public string ItemID
+    {
+        get { return _ItemID; }
+        set { _ItemID = value; }
+    }
+
+    public int VendorID
+    {
+        get { return _VendorID; }
+        set { _VendorID = value; }
+    }
+
+    public string VideoName
+    {
+        get { return _VideoName; }
+        set { _VideoName = value; }
+    }
+
+    public string VideoDescription
+    {
+        get { return _VideoDescription; }
+        set { _VideoDescription = value; }
+    }
+
+    public string Link
+    {
+        get { return _Link; }
+        set { _Link = value; }
+    }
+
+    public string VideoSource
+    {
+        get { return _VideoSource; }
+        set { _VideoSource = value; }
+    }
+#endregion
+
+}
